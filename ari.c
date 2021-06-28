6bd12d8fcdc093c3aa46cdc485a9f67e81680930
Jose Tom Vettom <84711737+jtv0013@users.noreply.github.com>
2021-06-28 21:13:51 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -29,82 +29,81 @@ string r;
 switch(ari);
 {
 case 1:	
-{
+
 r= "Kindergarten";
 break;
-}
+
 case 2:	
-{
+
 r= "First/Second Grade";
 break;
-}
+
 case 3:	
-{
+
 r= "Third Grade";
 break;
-}
+
 case 4:	
-{	
+	
 r= "Fourth Grade";
 break;
-}
+
 case 5:
-{
 
 r= "Fifth Grade";
 break;
-}
+
 case 6:	
-{
+
 r= "Sixth Grade";
 break;
-}
+
 case 7:	
-{
+
 r= "Seventh Grade";
 
 break;
-}
+
 case 8:	
-{
+
 r= "Eighth Grade";
 break;
-}
+
 case 9:	
-{
+
 r= "Ninth Grade";
 break;
-}
+
 case 10:	
-{
+
 r= "Tenth Grade";
 break;
-}
+
 case 11:	
-{
+
 r= "Eleventh Grade";
 break;
-}
+
 case 12:	
-{
+
 r= "Twelfth grade";
 break;
-}
+
 case 13:	
-{
+
 r= "College student";
 break;
-}	
+	
 case 14:	
-{
+
 r= "Professor";
 break;
-}
+
 default : 
-{
+
 r= "Extraordinaire";
 break;
-}
+
 }
 	
 return r;

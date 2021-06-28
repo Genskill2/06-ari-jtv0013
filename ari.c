0acc0dc5b5b88e9695564b5730fc1e75d8ec1afc
Jose Tom Vettom <84711737+jtv0013@users.noreply.github.com>
2021-06-28 21:36:26 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -27,49 +27,50 @@ se++;
 aria=4.71*((float)c/(float)w)+0.5*((float)w/(float)se)-21.43;
 int ari=((int)(ceil(aria)));
 string r;
-switch(ari){
-     case 1:
-        r="Kindergarten";
-        break;
-     case 2:
-        r="First/Second Grade";
-        break;
-     case 3: 
-        r="Third Grade";
-        break; 
-     case 4:
-        r="Fourth Grade";
-        break;   
-     case 5:
-        r="Fifth Grade";
-        break;  
-     case 6:
-        r="Sixth Grade";
-        break;
-     case 7:
-        r="Seventh Grade";
-        break;
-     case 8:
-        r="Eigth Grade";
-        break;
-     case 9:
-        r="Ninth Grade";
-        break;
-     case 10:
-        r="Tenth Grade";
-        break;
-     case 11:
-        r="Eleventh Grade";
-        break;
-     case 12:
-        r="Twelfth Grade";
-        break;
-     case 13:
-        r="College student";
-        break;
-     case 14:
-        r="Professor";
-        break;
-   }
+switch(ari)
+{
+case 1:
+r="Kindergarten";
+break;
+case 2:
+r="First/Second Grade";
+break;
+case 3: 
+r="Third Grade";
+break; 
+case 4:
+r="Fourth Grade";
+break;   
+case 5:
+r="Fifth Grade";
+break;  
+case 6:
+r="Sixth Grade";
+break;
+case 7:
+r="Seventh Grade";
+break;
+case 8:
+r="Eigth Grade";
+break;
+case 9:
+r="Ninth Grade";
+break;
+case 10:
+r="Tenth Grade";
+break;
+case 11:
+r="Eleventh Grade";
+break;
+case 12:
+r="Twelfth Grade";
+break;
+case 13:
+r="College student";
+break;
+case 14:
+r="Professor";
+break;
+}
 return r;
 }
